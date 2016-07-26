0251c1b86d4fa3d685999d7dbc0600f4b3bb4d68
Chris Thielen <github@sandgnat.com>
2016-07-25 20:29:13 -0500

    feat: Support custom function for H[1-6] innerHTML generation

--- src/linkjuice.js
+++ src/linkjuice.js
@@ -1,26 +1,26 @@
 let scope, nodes, inject;
 
-const wrapNode = node => {
-  let a = document.createElement('a');
-  a.href = `#${node.id}`;
-  if(!a.href) {
-      console.warn('No ID for element', a);
-      return ;
+const makeLink = (node, inject) => `
+    <a class="linkjuice" id="${node.id}">
+      <span class="linkjuice-icon">${inject}</span>${node.innerHTML}
+    </a>`;
+    
+const wrapNode = (node, contentFn) => {
+  if (!node.id) {
+    console.warn('No ID for element', node);
+    return;
   }
-  a.className = 'linkjuice';
-  a.innerHTML = `<span class="linkjuice-icon">${inject}</span>${node.innerHTML}`;
-  node.innerHTML = '';
-  node.appendChild(a);
+  node.innerHTML = contentFn(node, inject);
 };
 
-const linkjuice = (mount, { icon = '#', selectors = ['h2', 'h3', 'h4', 'h5', 'h6']}) => {
+const linkjuice = (mount, { contentFn = makeLink, icon = '#', selectors = ['h2', 'h3', 'h4', 'h5', 'h6']}) => {
   scope = document.querySelector(mount);
   if (!scope) return;
   inject = icon;
   nodes = scope.querySelectorAll((selectors).join(','));
   for (var i = nodes.length; i--;) {
-    wrapNode(nodes[i]);
+    wrapNode(nodes[i], contentFn);
   }
 };
 
-export const init = (mount, options) => linkjuice(mount, options);
+export const init = (mount, options = {}) => linkjuice(mount, options);
