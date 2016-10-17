65b0e451490bdba50908b22c546f91d97cf84dbd
Klaas Cuvelier <cuvelierklaas@gmail.com>
2016-10-17 15:25:52 -0700

    Add table of contents

--- dist/linkjuice.js
+++ dist/linkjuice.js
@@ -17,12 +17,23 @@
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
+
+  var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) {
+    return typeof obj;
+  } : function (obj) {
+    return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
+  };
+
   var scope = void 0,
       nodes = void 0,
       inject = void 0;
 
   var makeLink = function makeLink(node, inject) {
     return '\n    <a class="linkjuice" id="' + node.id + '">\n      <span class="linkjuice-icon">' + inject + '</span>' + node.innerHTML + '\n    </a>';
+  };
+
+  var makeTocItem = function makeTocItem(node) {
+    return '<a href="#' + node.id + '">' + node.querySelector('a').innerHTML + '</a>';
   };
 
   var wrapNode = function wrapNode(node, contentFn) {
@@ -33,6 +44,46 @@
     node.innerHTML = contentFn(node, inject);
   };
 
+  var addAtLevel = function addAtLevel(nodeLevel, node, tree) {
+    var level = tree;
+
+    while (--nodeLevel >= 0) {
+      if (!Array.isArray(level[level.length - 1])) {
+        level.push([]);
+      }
+
+      level = level[level.length - 1];
+    }
+
+    level.push(node);
+  };
+
+  var createTree = function createTree(nodes) {
+    var headerRegexp = /^H[1-6]$/;
+    var tree = [];
+
+    if (nodes.filter(function (node) {
+      return !headerRegexp.test(node.tagName);
+    }).length > 0) {
+      console.warn('Nested table of contents is only possible with regular header tags');
+      return tree;
+    }
+
+    nodes.forEach(function (node) {
+      return addAtLevel(parseInt(node.tagName.substr(1), 10) - 1, node, tree);
+    });
+    return tree;
+  };
+
+  var createTocHtml = function createTocHtml(nodes, contentFn) {
+
+    var nodesHtml = nodes.map(function (node) {
+      return Array.isArray(node) ? createTocHtml(node, contentFn) : '<li>' + contentFn(node) + '</li>';
+    }).join('');
+
+    return '<ul>' + nodesHtml + '</ul>';
+  };
+
   var linkjuice = function linkjuice(mount, _ref) {
     var _ref$contentFn = _ref.contentFn;
     var contentFn = _ref$contentFn === undefined ? makeLink : _ref$contentFn;
@@ -40,18 +91,36 @@
     var icon = _ref$icon === undefined ? '#' : _ref$icon;
     var _ref$selectors = _ref.selectors;
     var selectors = _ref$selectors === undefined ? ['h2', 'h3', 'h4', 'h5', 'h6'] : _ref$selectors;
+    var _ref$tableOfContents = _ref.tableOfContents;
+    var tableOfContents = _ref$tableOfContents === undefined ? false : _ref$tableOfContents;
 
     scope = document.querySelector(mount);
     if (!scope) return;
+
     inject = icon;
-    nodes = scope.querySelectorAll(selectors.join(','));
-    for (var i = nodes.length; i--;) {
-      wrapNode(nodes[i], contentFn);
+    nodes = Array.prototype.slice.call(scope.querySelectorAll(selectors.join(',')));
+
+    nodes.map(function (node) {
+      return wrapNode(node, contentFn);
+    });
+
+    if ((typeof tableOfContents === 'undefined' ? 'undefined' : _typeof(tableOfContents)) === 'object' && tableOfContents !== null) {
+      var toc = document.querySelector(tableOfContents.selector);
+
+      if (!toc) {
+        console.warn('No table of contents element found for the specified selector');
+        return;
+      }
+
+      var tocElements = tableOfContents.nested === true ? createTree(nodes) : nodes;
+      var createTocItem = typeof tableOfContents.contentFn === 'function' ? tableOfContents.contentFn : makeTocItem;
+
+      toc.innerHTML = createTocHtml(tocElements, createTocItem);
     }
   };
 
   var init = exports.init = function init(mount) {
-    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
     return linkjuice(mount, options);
   };
 });
--- src/linkjuice.js
+++ src/linkjuice.js
@@ -4,7 +4,9 @@ const makeLink = (node, inject) => `
     <a class="linkjuice" id="${node.id}">
       <span class="linkjuice-icon">${inject}</span>${node.innerHTML}
     </a>`;
-    
+
+const makeTocItem = node => `<a href="#${node.id}">${node.querySelector('a').innerHTML}</a>`;
+
 const wrapNode = (node, contentFn) => {
   if (!node.id) {
     console.warn('No ID for element', node);
@@ -13,13 +15,63 @@ const wrapNode = (node, contentFn) => {
   node.innerHTML = contentFn(node, inject);
 };
 
-const linkjuice = (mount, { contentFn = makeLink, icon = '#', selectors = ['h2', 'h3', 'h4', 'h5', 'h6']}) => {
+const addAtLevel = (nodeLevel, node, tree) => {
+  let level = tree;
+
+  while (--nodeLevel >= 0) {
+    if (!Array.isArray(level[level.length - 1])) {
+      level.push([]);
+    }
+
+    level = level[level.length - 1];
+  }
+
+  level.push(node);
+};
+
+const createTree = nodes => {
+  const headerRegexp = /^H[1-6]$/;
+  const tree = [];
+
+  if (nodes.filter(node => !headerRegexp.test(node.tagName)).length > 0) {
+    console.warn('Nested table of contents is only possible with regular header tags');
+    return tree;
+  }
+
+  nodes.forEach(node => addAtLevel(parseInt(node.tagName.substr(1), 10) - 1, node, tree));
+  return tree;
+};
+
+const createTocHtml = (nodes, contentFn) => {
+
+  const nodesHtml = nodes.map(node => Array.isArray(node) ? createTocHtml(node, contentFn) : `<li>${contentFn(node)}</li>`
+  ).join('');
+
+  return `<ul>${nodesHtml}</ul>`;
+
+};
+
+const linkjuice = (mount, { contentFn = makeLink, icon = '#', selectors = ['h2', 'h3', 'h4', 'h5', 'h6'], tableOfContents = false}) => {
   scope = document.querySelector(mount);
   if (!scope) return;
+
   inject = icon;
-  nodes = scope.querySelectorAll((selectors).join(','));
-  for (var i = nodes.length; i--;) {
-    wrapNode(nodes[i], contentFn);
+  nodes = Array.prototype.slice.call(scope.querySelectorAll((selectors).join(',')));
+
+  nodes.map(node => wrapNode(node, contentFn));
+
+  if (typeof tableOfContents === 'object' && tableOfContents !== null) {
+    const toc = document.querySelector(tableOfContents.selector);
+
+    if (!toc) {
+      console.warn('No table of contents element found for the specified selector');
+      return;
+    }
+
+    const tocElements = tableOfContents.nested === true ? createTree(nodes) : nodes;
+    const createTocItem = typeof tableOfContents.contentFn === 'function' ? tableOfContents.contentFn : makeTocItem;
+
+    toc.innerHTML = createTocHtml(tocElements, createTocItem);
   }
 };
 
